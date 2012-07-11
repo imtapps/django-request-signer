0b1cc572c912ffecf77c95d2645d2462be25cf72
amadison <aaron.madison@imtapps.com>
2012-07-11 15:51:02 -0500

    changing ci stuff...

--- setup.py
+++ setup.py
@@ -1,49 +1,28 @@
-import os
-from distutils.core import Command
-from setuptools import find_packages, setup
+
+from setuptools import setup, find_packages
 
 from request_signer import VERSION
 
-REQUIREMENTS = ['Django', 'south', 'apysigner']
+setup(
+    name='django-request-signer',
+    version=VERSION,
+    author='imtapps',
+    url='https://github.com/imtapps/django-request-signer',
+    description="A python library for signing http requests.",
+    long_description=open('README.rst', 'r').read(),
+    install_requires=file('requirements/dist.txt').read().split("\n"),
+    tests_require=file('requirements/test.txt').read().split("\n"),
+    packages=find_packages(exclude=["example", "request_signer.tests"]),
 
-def do_setup():
-    setup(
-        name='django-request-signer',
-        version=VERSION,
-        author='imtapps',
-        url='https://github.com/imtapps/django-request-signer',
-        description="A python library for signing http requests.",
-        long_description=open('README.rst', 'r').read(),
-        install_requires=REQUIREMENTS,
-        packages=find_packages(exclude=['example']),
-        cmdclass={
-            'install_dev': InstallDependencies,
-        },
-        classifiers=[
-            'Development Status :: 5 - Production/Stable',
-            'Intended Audience :: Developers',
-            'Natural Language :: English',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python :: 2',
-            'License :: OSI Approved :: BSD License',
-            'Topic :: Internet',
-            'Topic :: Internet :: WWW/HTTP',
-            'Topic :: Software Development :: Libraries :: Python Modules',
-        ],
-    )
-
-class InstallDependencies(Command):
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        os.system("pip install %s -i http://chishop.apps-system.com" % ' '.join(REQUIREMENTS))
-        os.system("pip install -r test_requirements.txt  -i http://chishop.apps-system.com")
-
-if __name__ == '__main__':
-    do_setup()
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Internet',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
