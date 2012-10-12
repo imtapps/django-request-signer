bb38df1332da03e370ffdd9fbb9249da4c55c3a5
Henry <toran.billups@theimtgroup.com>
2012-10-12 10:33:45 -0500

    bumped version and removed check signature (it now gets it from generic request signer)

--- request_signer/decorators.py
+++ request_signer/decorators.py
@@ -1,46 +1,11 @@
-
 import functools
 import json
 
-import apysigner
-
 from django import http
-from django.utils.crypto import constant_time_compare
 from django.views.decorators.csrf import csrf_exempt
-import re
+from generic_request_signer.check_signature import check_signature
 
 from request_signer import  constants, models
-
-
-def check_signature(signature, private_key, full_path, payload):
-    """
-    Checks signature received and verifies that we are able to re-create
-    it from the private key, path, and payload given.
-
-    :param signature:
-        Signature received from request.
-    :param private_key:
-        Base 64, url encoded private key.
-    :full_path:
-        Full path of request, including GET query string
-    :payload:
-        The request.POST data if present. None if not.
-
-    :returns:
-        Boolean of whether signature matched or not.
-    """
-    url_to_check = _strip_signature_from_url(signature, full_path)
-    computed_signature = apysigner.get_signature(private_key, url_to_check, payload)
-    return constant_time_compare(signature, computed_signature)
-
-def _strip_signature_from_url(signature, url_path):
-    """
-    Strips signature off path. Can show up either as the first parameter
-    or as an additional parameter (leading '?' or '&')
-    """
-    signature_qs = r"(\?|&)?{0}={1}$".format(constants.SIGNATURE_PARAM_NAME, signature)
-    clean_url = re.sub(signature_qs, '', url_path, count=1)
-    return clean_url
 
 
 def signature_required(func):
--- request_signer/tests/signed_request_tests.py
+++ request_signer/tests/signed_request_tests.py
@@ -5,11 +5,10 @@ from django import test
 from django import http
 
 from request_signer import  models, constants
-from request_signer.decorators import signature_required, check_signature
+from request_signer.decorators import signature_required
 
 __all__ = (
     'SignedRequestTests',
-    'CheckSignatureTests',
 )
 
 
@@ -179,50 +178,3 @@ class SignedRequestTests(test.TestCase):
     def test_signed_views_are_csrf_exempt(self):
         signed_view = signature_required(self.view)
         self.assertTrue(getattr(signed_view, 'csrf_exempt', False))
-
-
-
-class CheckSignatureTests(test.TestCase):
-
-    def setUp(self):
-        self.TEST_PRIVATE_KEY = 'UHJpdmF0ZSBLZXk='
-        self.url_one = "/example/get-it/?q=find"
-        self.url_two = "/example/post-it/"
-        self.url_post_data = {'q': 'update'}
-
-        # valid signatures for urls above and data
-        self.signature_one = "T-lT3uT2wpUobJvDkXpxtsEAl7KmrEg6k3So_Varya8="
-        self.signature_two = "AKm9ZGCZPaYRMnLxpFZ8-ulaCIr_wKYAruZVm36uv3Q="
-
-    def test_returns_true_when_signatures_match_and_no_post_data(self):
-        signature_valid = check_signature(self.signature_one, self.TEST_PRIVATE_KEY, self.url_one, None)
-        self.assertEqual(True, signature_valid)
-
-    def test_returns_true_when_signatures_match_and_has_post_data(self):
-        signature_valid = check_signature(self.signature_two, self.TEST_PRIVATE_KEY, self.url_two, self.url_post_data)
-        self.assertEqual(True, signature_valid)
-
-    def test_returns_false_when_signatures_dont_match(self):
-        bad_signature = "ABCc0hS02rVC3016krevud1aW9B6Ls0Tp4_XcezuXYZ="
-        signature_valid = check_signature(bad_signature, self.TEST_PRIVATE_KEY, self.url_one, None)
-        self.assertEqual(False, signature_valid)
-
-    def test_doesnt_use_signature_already_in_url_to_check_for_valid_signature(self):
-        url_with_sig = "{url}&{signature_param}={signature}".format(
-            url=self.url_one,
-            signature_param=constants.SIGNATURE_PARAM_NAME,
-            signature=self.signature_one,
-        )
-
-        signature_valid = check_signature(self.signature_one, self.TEST_PRIVATE_KEY, url_with_sig, None)
-        self.assertEqual(True, signature_valid)
-
-    def test_doesnt_use_signature_already_in_url_to_check_for_valid_signature_with_post_data(self):
-        url_with_sig = "{url}?{signature_param}={signature}".format(
-            url=self.url_two,
-            signature_param=constants.SIGNATURE_PARAM_NAME,
-            signature=self.signature_two,
-        )
-
-        signature_valid = check_signature(self.signature_two, self.TEST_PRIVATE_KEY, url_with_sig, self.url_post_data)
-        self.assertEqual(True, signature_valid)
