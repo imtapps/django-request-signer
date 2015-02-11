b4505b3f1cf1cb476757e29abe94871d23a73676
Taylor Hobbs <taylor@feemon.com>
2015-02-11 13:57:12 -0600

    Version: 0.7.2 -> 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-request-signer',
-    version='0.7.2',
+    version='1.0.0',
     author='imtapps',
     url='https://github.com/imtapps/django-request-signer',
     description="A python library for signing http requests.",
