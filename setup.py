df89055579d67672eb6f5bb53afa9f54d93c28ab
JarrodCTaylor <jarrod.c.taylor@gmail.com>
2015-08-03 15:32:26 -0500

    1.0.0 ~> 1.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-request-signer',
-    version='1.0.1',
+    version='1.1.0',
     author='imtapps',
     url='https://github.com/imtapps/django-request-signer',
     description="A python library for signing http requests.",
