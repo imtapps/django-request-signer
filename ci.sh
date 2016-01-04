f1ea800ac7a5cf8de2f3d29b9f124e950bbf0f23
TayHobbs <hobbstay@gmail.com>
2016-01-04 13:34:59 -0600

    Peripheral: Added tox build for Python 2.7 and 3.4

--- ci.sh
+++ ci.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# verify user provided a name for the virtualenv
 if [ -z "$1" ]; then
     echo "usage: $0 virtual_env_name"
     exit
 fi
+
 VIRTUALENV_NAME=$1
 
 virtualenv $VIRTUALENV_NAME
@@ -11,16 +13,28 @@ virtualenv $VIRTUALENV_NAME
 
 find . -name "*.pyc" -delete
 
-pip install -r requirements/test.txt
+pip install tox
 
-python manage.py test
+tox
 
-flake8 request_signer --max-line-length=120 --max-complexity=5  > flake8.txt
-FLAKE8_EXIT=`cat flake8.txt | wc -l`
+TEST_EXIT=$?
+rm -rf jenkins_reports
+mkdir jenkins_reports
+pep8 request_signer > jenkins_reports/pep8.report
+PEP8_EXIT=$?
+pyflakes request_signer > jenkins_reports/pyflakes.report
+PYFLAKES_EXIT=$?
 
-EXIT=`expr $FLAKE8_EXIT`
-if [ $EXIT -gt 1 ]; then
-    echo "FLAKE8 exit status:" $FLAKE8_EXIT
+# cleanup virtualenv
+deactivate
+rm -rf $VIRTUALENV_NAME
+
+let JENKINS_EXIT="$TEST_EXIT + $PEP8_EXIT + $PYFLAKES_EXIT"
+if [ $JENKINS_EXIT -gt 2 ]; then
+    echo "Test exit status:" $TEST_EXIT
+    echo "PEP8 exit status:" $PEP8_EXIT
+    echo "Pyflakes exit status:" $PYFLAKES_EXIT
     echo "Exiting Build with status:" $EXIT
-    exit $EXIT
+    exit $JENKINS_EXIT
 fi
+
