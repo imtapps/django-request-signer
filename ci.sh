8696366f3a322765d27b1e7cb3804caeeeaf2fb9
Kyle Hornberg <kyle.hornberg@gmail.com>
2016-04-14 12:30:58 -0500

    Peripheral: Display flake8 results

--- ci.sh
+++ ci.sh
@@ -5,6 +5,7 @@ TEST_EXIT=$?
 
 echo "Flake8 Results"
 flake8 request_signer --max-complexity=5 --max-line-length=120 --exclude=base.py,"*/client/generic/__init__.py" > flake8.txt
+cat ./flake8.txt
 FLAKE8_EXIT=`cat ./flake8.txt | wc -l`
 
 let JENKINS_EXIT="$TEST_EXIT + $FLAKE8_EXIT"
