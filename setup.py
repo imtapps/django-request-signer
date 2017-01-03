54deecb0dd6c22fe74c8a408a31f6a139990e6ad
TayHobbs <hobbstay@gmail.com>
2017-01-03 13:40:52 -0600

    Version: 3.2.3 ~> 3.3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-request-signer',
-    version='3.2.3',
+    version='3.3.0',
     author='imtapps',
     url='https://github.com/imtapps/django-request-signer',
     description="A python library for signing http requests.",
